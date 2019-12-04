de573140371c86d09573cfb691ecd11888b4dbd8
wellington <wellingtonslpbi@hotmail.com>
2019-12-04 08:50:58 -0300

    creating ask page

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/ask', (req, res) => {
+  res.render('ask')
+})
+
+app.listen(3000, () => {
+  console.log(`Server is running on port: 3000`)
+});
